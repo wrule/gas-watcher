caa06d85501719452d89c5df4d019f71662c4a19
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-13 18:49:39 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,13 @@ async function hello() {
   console.log(wallet.address);
   const balance = await provider.getBalance(wallet.address);
   console.log(formatEther(balance));
-  
+
+  const erc20 = new Contract('0x3597bfD533a99c9aa083587B074434E61Eb0A258', [
+    'function approve(address, uint256) returns (bool)'
+  ], wallet);
+  const actionGas = await erc20.approve.estimateGas('0x3597bfD533a99c9aa083587B074434E61Eb0A258', 1n);
+  console.log(actionGas);
+
   watch(async () => {
     const fee = await provider.getFeeData();
     if (fee.gasPrice != null && fee.maxPriorityFeePerGas != null) {
