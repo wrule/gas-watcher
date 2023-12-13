57581316f931c4bdf9f75ee9ea7303ed0fb89faa
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-13 17:46:35 +0800

    转账的gas计算成功

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ import ethers, { Wallet, Contract, JsonRpcProvider, parseEther, formatEther, par
 
 const secret = require('../.secret.json');
 
+async function watch(func: () => any, interval = 2000) {
+  try {
+    await func();
+  } catch (error) {
+    console.error(error);
+  }
+  setTimeout(() => {
+    watch(func, interval);
+  }, interval);
+}
+
 export
 async function hello() {
   const provider = new JsonRpcProvider(secret.alchemyKey);
@@ -9,4 +20,11 @@ async function hello() {
   console.log(wallet.address);
   const balance = await provider.getBalance(wallet.address);
   console.log(formatEther(balance));
+  
+  watch(async () => {
+    const fee = await provider.getFeeData();
+    if (fee.gasPrice != null && fee.maxPriorityFeePerGas != null) {
+      console.log(formatEther((fee.gasPrice + fee.maxPriorityFeePerGas) * 21000n));
+    }
+  });
 }
