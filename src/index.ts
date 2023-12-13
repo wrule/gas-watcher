d1924066aab06e949fca7a1fc5ac8c49422ccd0a
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-13 15:06:42 +0800

    获取余额度

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import ethers from 'ethers';
+import ethers, { Wallet, JsonRpcProvider, parseEther, formatEther } from 'ethers';
+
+const secret = require('../.secret.json');
 
 export
-function hello() {
-  console.log('hello world!');
+async function hello() {
+  const provider = new JsonRpcProvider(secret.alchemyKey);
+  const wallet = new Wallet(secret.privateKey, provider);
+  console.log(wallet.address);
+  const balance = await provider.getBalance('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
+  console.log(formatEther(balance));
 }
