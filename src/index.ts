1fa5bcd19b0fb32e0b87776421ad6d3789f6f20f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-12-13 16:10:19 +0800

    需要

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ const secret = require('../.secret.json');
 
 export
 async function hello() {
+  console.log(formatEther('3340656982859224'));
   const provider = new JsonRpcProvider(secret.alchemyKey);
   const wallet = new Wallet(secret.privateKey, provider);
   console.log(wallet.address);
-  const balance = await provider.getBalance('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
-  console.log(formatEther(balance));
+  const balance = await provider.getBalance('0x7F3b3C3f7e69E91D95a32f0A054B8C1B3167865C');
+  console.log(balance);
   const erc20 = new Contract('0x3597bfd533a99c9aa083587b074434e61eb0a258', [
     'function decimals() view returns (uint256)',
     'function symbol() view returns (string)',
@@ -19,14 +20,15 @@ async function hello() {
   const value = parseUnits('2', decimals);
   const tx = await erc20.approve.estimateGas('0x3597bfd533a99c9aa083587b074434e61eb0a258', value);
   console.log(tx);
-  setInterval(async () => {
-    const [gas, { gasPrice, maxPriorityFeePerGas, maxFeePerGas }] = await Promise.all([
-      erc20.approve.estimateGas('0x3597bfd533a99c9aa083587b074434e61eb0a258', value),
-      provider.getFeeData(),
-    ]);
-    if (maxFeePerGas != null && gasPrice != null && maxPriorityFeePerGas != null) {
-      // console.log(formatUnits(maxPriorityFeePerGas, 'gwei'));
-      console.log(formatEther(gas * (maxFeePerGas)));
-    }
-  }, 2000);
+  await erc20.approve('0x3597bfd533a99c9aa083587b074434e61eb0a258', value);
+  // setInterval(async () => {
+  //   const [gas, { gasPrice, maxPriorityFeePerGas, maxFeePerGas }] = await Promise.all([
+  //     erc20.approve.estimateGas('0x3597bfd533a99c9aa083587b074434e61eb0a258', value),
+  //     provider.getFeeData(),
+  //   ]);
+  //   if (maxFeePerGas != null && gasPrice != null && maxPriorityFeePerGas != null) {
+  //     // console.log(formatUnits(maxPriorityFeePerGas, 'gwei'));
+  //     console.log(formatEther(gas * (gasPrice + maxPriorityFeePerGas)));
+  //   }
+  // }, 2000);
 }
