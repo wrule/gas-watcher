4fb0e576e964c21d77e02c51a064dbc0a7117e85
jimao <jimao@jimaodeMacBook-Pro.local>
2024-01-03 13:22:47 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import ethers, { Wallet, Contract, JsonRpcProvider, parseEther, formatEther, parseUnits, formatUnits } from 'ethers';
+import ethers, { Wallet, HDNodeWallet, Contract, JsonRpcProvider, parseEther, formatEther, parseUnits, formatUnits, Mnemonic } from 'ethers';
 
 (BigInt.prototype as any).toJSON = function() { return this.toString(); };
 const secret = require('../.secret.json');
@@ -16,11 +16,13 @@ async function watch(func: () => any, interval = 2000) {
 
 export
 async function hello() {
-  const provider = new JsonRpcProvider(secret.alchemyKey);
-  watch(async () => {
-    const fee = await provider.getFeeData();
-    console.log(
-      `${JSON.stringify([Date.now() ,fee.gasPrice, fee.maxPriorityFeePerGas, fee.maxFeePerGas])},`
-    );
-  });
+  const mnemonic = Mnemonic.fromPhrase(secret.phrase);
+  const addrList = Array(200).fill(0).map((_, index) =>
+    HDNodeWallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${index}`).address
+  );
+  for (let i = 0; i < addrList.length; ++i) {
+    console.log('index', i);
+    const address = addrList[i];
+    console.log(address);
+  }
 }
